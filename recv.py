22fbb4041356c1c80764c070bb286bcf28a7e335
Fran6nd aka Francois NOUAILLE-DEGORCE <my@secret.email>
2022-06-21 20:40:03 +0200

    Added small interface for receiving text.

--- recv.py
+++ recv.py
@@ -3,6 +3,7 @@
 import sounddevice as sd
 import numpy as np
 import time
+import threading
 
 from morse import Morse
 
@@ -16,12 +17,13 @@ class MorseRecv:
     current_status = 0
     output = ''
     volume_norm = 0
+    squelch = 5
 
     def print_sound(indata, outdata, frames, time, status):
         if MorseRecv.running:
             MorseRecv.volume_norm = np.linalg.norm(indata)*10
             delay = MorseRecv.millis() - MorseRecv.status_last_change
-            if MorseRecv.volume_norm > 5:
+            if MorseRecv.volume_norm > MorseRecv.squelch:
                 MorseRecv.current_status = 1
             else:
                 MorseRecv.current_status = 0
@@ -42,24 +44,43 @@ class MorseRecv:
 
             MorseRecv.last_status = MorseRecv.current_status
         
-
-            if delay > 4000 and MorseRecv.current_status == 0:
-                MorseRecv.running = False
+            # Useful for self shutdown.
+            # if delay > 4000 and MorseRecv.current_status == 0:
+            #    MorseRecv.running = False
             
         #print ("|" * int(volume_norm))
 
     def recv():
+        MorseRecv.running = True
         with sd.Stream(callback=MorseRecv.print_sound):
-            MorseRecv.running = True
-            #sd.sleep(20000)
+        #    MorseRecv.running = True
+            
             #wait for [return] key.
             #input()
             while(MorseRecv.running):
-                time.sleep(1)
-                print("|" * int(MorseRecv.volume_norm))
-        return MorseRecv.output
+                sd.sleep(100)
+        #        print("|" * int(MorseRecv.volume_norm))
+        #return MorseRecv.output
 
 if __name__ == '__main__':
-    msg = MorseRecv.recv()
-    print(msg)
-    print(Morse.to_text(msg))
+    import curses
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+
+    stdscr.nodelay(1)
+    #MorseRecv.recv()
+    t = threading.Thread(target=MorseRecv.recv)
+    t.start()
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 0, MorseRecv.output + '\n' +  Morse.to_text(MorseRecv.output) + '\n' +  '|' * int(MorseRecv.volume_norm) + '\n' + ' ' * (MorseRecv.squelch-1) + '^')
+        c = stdscr.getch()
+        if c == ord('q'):
+            break  # Exit the while loop
+        elif c == curses.KEY_HOME:
+            x = y = 0
+    MorseRecv.running = False
+    curses.endwin()
+    t.join()
